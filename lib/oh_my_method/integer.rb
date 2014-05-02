b2478c452035c4f722599f3fbdef08a941dfe8cd
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-03 02:49:24 +0900

    Create factorial method

--- lib/oh_my_method/integer.rb
+++ lib/oh_my_method/integer.rb
@@ -6,4 +6,9 @@ class Integer
   def minus?
     self < 0
   end
+
+  def factorial
+    return 1 if self.zero?
+    (1..self).inject(:*)
+  end
 end
--- spec/oh_my_method/integer_spec.rb
+++ spec/oh_my_method/integer_spec.rb
@@ -1,17 +1,23 @@
 require 'spec_helper'
 
 describe "Integer" do
-  it "plus?" do
+  it "#plus?" do
     expect(-1.plus?).to eq false
     expect(0.plus?).to eq false
     expect(1.plus?).to eq true
     expect((1<<64).plus?).to eq true
   end
 
-  it "minus?" do
+  it "#minus?" do
     expect(-1.minus?).to eq true
     expect(0.minus?).to eq false
     expect(1.minus?).to eq false
     expect((1<<64).minus?).to eq false
+  end
+
+  it "#factorial" do
+    expect(0.factorial).to eq 1
+    expect(3.factorial).to eq 6
+    expect(10.factorial).to eq 3628800
   end
 end
