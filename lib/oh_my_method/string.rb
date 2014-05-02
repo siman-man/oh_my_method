dae6b2d64b48180c02f9abb6e03b3d78cbfbbff8
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-02 23:21:44 +0900

    Create middle_with? method

--- lib/oh_my_method/string.rb
+++ lib/oh_my_method/string.rb
@@ -2,4 +2,8 @@ class String
   def palindrome?
     self == self.reverse
   end
+
+  def middle_with?(str)
+    (self =~ /.+#{str}.+/)? true : false
+  end
 end
--- spec/oh_my_method/string_spec.rb
+++ spec/oh_my_method/string_spec.rb
@@ -7,4 +7,11 @@ describe "String Class" do
     expect("Heeeh".palindrome?).to eq false
     expect("ABCBA".palindrome?).to eq true
   end
+
+  it "middle_with?" do
+    expect("hello".middle_with?("ell")).to eq true
+    expect("hello".middle_with?("ello")).to eq false
+    expect("hello".middle_with?("hel")).to eq false
+    expect("ABABA".middle_with?("BAB")).to eq true
+  end
 end
