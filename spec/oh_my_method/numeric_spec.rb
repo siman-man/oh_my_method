ee05ae581280ee24e38202c4822318b97d4b7f0b
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-03 09:45:29 +0900

    Fix numeric_spec.rb

--- spec/oh_my_method/numeric_spec.rb
+++ spec/oh_my_method/numeric_spec.rb
@@ -4,11 +4,11 @@ describe "Numeric Class" do
   it "#notzero?" do
     expect(0.notzero?).to eq false
     expect((0.0).notzero?).to eq false
-    expect((0i).notzero?).to eq false
-    expect((0/4r).notzero?).to eq false
+    expect(Complex(0).notzero?).to eq false
+    expect(Rational(0,4).notzero?).to eq false
     expect(3.notzero?).to eq true
     expect((3.0).notzero?).to eq true
-    expect((3/4r).notzero?).to eq true
-    expect(4i.notzero?).to eq true
+    expect(Rational(3,4).notzero?).to eq true
+    expect(Complex(0,4).notzero?).to eq true
   end
 end
