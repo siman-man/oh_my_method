6416b47537d2ffca967bacc8c6f1ed6b1133e89a
siman-man <k128585@ie.u-ryukyu.ac.jp>
2014-05-04 16:57:05 +0900

    Add array.rb document

--- lib/oh_my_method/array.rb
+++ lib/oh_my_method/array.rb
@@ -1,4 +1,12 @@
 class Array
+  # return duplicate of result of Array#select.
+  #
+  # langs = [ "Ruby", "R", "Python", "Java", "JavaScript" ]
+  # dup = langs.deep_select{|lang| lang.first == "R"}
+  # dup.first.upcase!
+  #
+  # langs.first     #=> "Ruby"
+  # dup.first       #=> "RUBY"
   def deep_select
     block_given? or return enum_for(__method__)
 
@@ -9,6 +17,10 @@ class Array
     res
   end
 
+  # return the mean
+  # 
+  # array = (1..10).to_a
+  # array.mean    #=> 50.5
   def mean
     inject(:+)/size.to_f
   end
